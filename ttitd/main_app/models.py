c245584f4bdbf2e43a0aee3dbe30e3c7fbd52f33
Andrew Williamson <acwilliamson419@gmail.com>
2019-06-14 18:24:29 -0700

    match commit

--- ttitd/main_app/models.py
+++ ttitd/main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    gender = models.CharField(max_length=15)
+    age = models.SmallIntegerField()
+    weight = models.PositiveSmallIntegerField()
+    bio
+    social_link
+    exp
+    profile_photo
+    user_key
+    ghost_key

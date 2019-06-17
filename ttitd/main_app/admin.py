325e3b3118894501bacafccad3d74043e9577a5a
Andrew Williamson <acwilliamson419@gmail.com>
2019-06-17 15:35:58 -0700

    Updates for admin

--- ttitd/main_app/admin.py
+++ ttitd/main_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Drug, Effect #Experience, Photo
 
-# Register your models here.
+
 
 admin.site.register(Profile)
+admin.site.register(Drug)
+#admin.site.register(Experience)
+#admin.site.register(Photo)
+admin.site.register(Effect)

24acaefff74cc0fcbabd0b7245fe9a079a22bdd6
Timothy Floro <JIMDOTCF@Timothys-MacBook-Air-2.local>
2019-06-17 11:29:07 -0700

    adds signup

--- ttitd/main_app/views.py
+++ ttitd/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import User, Drug, Experience, Photo, 
+from .models import User, Profile 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -7,26 +7,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def signup(request):
-    error_message = ''
-    if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-        # This will add the user to the database
-        user = form.save()
-        # This is how we log a user in via code
-        login(request, user)
-        return redirect('index')
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
     else:
-        error_message = 'Invalid credentials - try again'
+      error_message = 'Invalid credentials - try again'
   # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
 
 def community(request):
   return render(request, 'community.html')

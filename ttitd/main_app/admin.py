a3b24d6cdae1feb06c7043b0e74d8a2ac7192624
Timothy Floro <JIMDOTCF@Timothys-Air-2.lan>
2019-06-16 19:10:40 -0700

    nav, login, and signup added

--- ttitd/main_app/admin.py
+++ ttitd/main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Drug, Experience, Photo
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Drug)
+admin.site.register(Experience)
+admin.site.register(Photo)

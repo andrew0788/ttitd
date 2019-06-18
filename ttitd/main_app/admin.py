fc6a90b5cc351636bd161bf8238d653c3b31db3b
Andrew Williamson <acwilliamson419@gmail.com>
2019-06-18 09:24:38 -0700

    Connects to AWS s3

--- ttitd/main_app/admin.py
+++ ttitd/main_app/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Drug, Experience, Photo
+from .models import Profile, Drug, Photo, Effect, user_drug_effects
 
 
 
 admin.site.register(Profile)
 admin.site.register(Drug)
-admin.site.register(Experience)
 admin.site.register(Photo)
-
+admin.site.register(Effect)
+admin.site.register(user_drug_effects)
--- ttitd/main_app/views.py
+++ ttitd/main_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import User, Drug, Experience, Photo, 
+from .models import User, Drug, Experience, Photo,
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'ttitd'
+
 # Create your views here.
+
 def signup(request):
     error_message = ''
     if request.method == 'POST':
@@ -34,3 +38,18 @@ def community(request):
 @login_required
 def profile(request):
   return render(request, 'profile.html')
+
+@login_required
+def add_photo(request, drug_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      photo = Photo(url=url, drug_id=drug_id)
+      photo.save()
+    except:
+      print('An error occurred uploading file to S3')
+  return redirect('detail', drug_id=drug_id)

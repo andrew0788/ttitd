ea43732424501da89b4617a7739facb04071a821
Timothy Floro <JIMDOTCF@Timothys-MacBook-Air-2.local>
2019-06-19 10:00:33 -0700

    adds hidden nav for mobile and desktop. updates to nav links.

--- ttitd/main_app/urls.py
+++ ttitd/main_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('profiles/detail', views.profile, name='profile'),
-    path('substances/all', views.substances_all, name='substances_all'),
-    path('trips/all', views.trips_all, name='trips_all'),
+    path('substances/index', views.substances_all, name='substances_all'),
+    path('trips/index', views.trips_all, name='trips_all'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup', views.signup, name='signup'),
 ]
--- ttitd/main_app/views.py
+++ ttitd/main_app/views.py
@@ -11,44 +11,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-p_user = User.id
-
-# users = User.objects.all().select_related(Profile.user_key)
-
-# def get_profile(request):
-#     if request.method == 'GET':
-#         p_form = ProfileForm()
-#     return render(request, 'signup.html', {'p_form': p_form})
-
-# def signup(request):
-#   # get_profile()
-#   error_message = ''
-#   form = UserCreationForm()
-#   if request.method == 'POST':
-#     user_form = UserCreationForm(request.POST)
-#     # profile_form = ProfileForm(request.POST, instance=request.user.profile)
-#     if user_form.is_valid():
-#       user_form = form.save()
-#       login(request, user)
-#       profile = Profile.objects.create(
-#           profile_name = request.user.get_username(),
-#           user_key = request.user,
-#       )
-#       profile.save()
-#       print("this worked")
-#       return redirect('index')
-#     else:
-#       error_message = 'Invalid credentials - try again'
-#   form = UserCreationForm()
-#   context = {'form': form, 'error_message': error_message}
-#   return render(request, 'registration/signup.html', context)
-
-
-
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'ttitd'
 
-# Create your views here.
+def home(request):
+  return render(request, 'home.html')
 
 def signup(request):
   error_message = ''
@@ -77,17 +44,15 @@ def form_valid(self, form):
   # Let the CreateView do its job as usual
   return super().form_valid(form)
 
-def home(request):
-  return render(request, 'home.html')
 
 def substances_all(request):
   d = Drug.objects.all()
-  return render(request, 'substances/all.html', {
+  return render(request, 'substances/index.html', {
     'd': d
   })
 
 def trips_all(request):
-  return render(request, 'trips/all.html')
+  return render(request, 'trips/index.html')
 
 
 @login_required

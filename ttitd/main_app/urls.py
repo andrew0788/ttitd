5a5caeb08f76c56940f6fea9a175efad0eba8010
Andrew Williamson <acwilliamson419@gmail.com>
2019-06-19 10:31:29 -0700

    removes 's'

--- ttitd/main_app/urls.py
+++ ttitd/main_app/urls.py
@@ -3,15 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('profiles/detail', views.profile, name='profile'),
-<<<<<<< Updated upstream
-    path('substances/index', views.substances_all, name='substances_all'),
-    path('trips/index', views.trips_all, name='trips_all'),
-=======
-    path('profiles/profile_update', views.profile_update, name="profile_update"),
-    path('substances/all', views.substances_all, name='substances_all'),
-    path('trips/all', views.trips_all, name='trips_all'),
->>>>>>> Stashed changes
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup', views.signup, name='signup'),
+    path('profiles/detail', views.profile, name='profile'),
+    path('profiles/profile_update', views.profile_update, name="profile_update"),
+    path('substances/index', views.substances_all, name='substances_all'),
+    path('trips/index', views.trips_all, name='trips_all'),
 ]

5ba4cf6f6df7f7c754c68a405dc207f847218b08
Andrew Williamson <acwilliamson419@gmail.com>
2019-06-18 18:06:56 -0700

    Formatting update

--- ttitd/main_app/urls.py
+++ ttitd/main_app/urls.py
@@ -3,41 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
     path('profiles/detail', views.profile, name='profile'),
     path('substances/all', views.substances_all, name='substances_all'),
     path('trips/all', views.trips_all, name='trips_all'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup', views.signup, name='signup'),
 ]
-
-
-
-
-
-
-s
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-s

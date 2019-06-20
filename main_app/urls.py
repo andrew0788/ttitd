94a8167d11c13f955d8cee08b855d4ee84ee6532
Andrew Williamson <acwilliamson419@gmail.com>
2019-06-19 21:19:24 -0700

    fix

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,11 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-<<<<<<< Updated upstream:main_app/urls.py
-=======
-    path('profiles/detail', views.profile, name='profile'),
-    path('profiles/profile_update', views.profile_update, name="profile_update"),
->>>>>>> Stashed changes:ttitd/main_app/urls.py
     path('substances/all', views.substances_all, name='substances_all'),
     path('substances/index', views.substances_all, name='substances_all'),
     path('trips/all', views.trips_all, name='trips_all'),

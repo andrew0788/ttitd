8b43e33139417b5cd5acab4c7bc9e6da5d427424
Andrew Williamson <acwilliamson419@gmail.com>
2019-06-19 20:50:37 -0700

    typo fix

--- main_app/views.py
+++ main_app/views.py
@@ -42,23 +42,6 @@ def signup(request):
   context = {'user_form': user_form}
   return render(request, 'registration/signup.html', context)
 
-# @login_required
-# def profile_update(request):
-#     if request.method == 'POST':
-#         profile_form = ProfileForm(request.POST)
-#         if profile_form.is_valid():
-#             user = profile_form.save()
-#             return redirect('/')
-#         else:
-#             error_message = 'Invalid credentials -- try again'
-#     else:
-#         profile_form = ProfileForm()
-#
-#     return render(request, 'profile/profile_update.html', {'profile_form': profile_form})
-#
-# def update_profile(request, user_id):
-#     user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
-#     user.save()
 
 @login_required
 @transaction.atomic
@@ -82,7 +65,7 @@ def profile_update(request):
 
 def substances_all(request):
   substance = Drug.objects.all()
-  return render(request, 'substances/all.html', {
+  return render(request, 'substances/index.html', {
     'substance': substance
   })
 

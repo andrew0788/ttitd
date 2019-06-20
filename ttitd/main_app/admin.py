65adcdd98035fe9cfe7e279562d5f5664dcf5cae
Andrew Williamson <acwilliamson419@gmail.com>
2019-06-19 20:21:01 -0700

    heroku commit

--- ttitd/main_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Drug, Photo, Effect, User_Drug_Effects
-
-admin.site.register(Profile)
-admin.site.register(Drug)
-admin.site.register(Photo)
-admin.site.register(Effect)
-admin.site.register(User_Drug_Effects)
--- ttitd/main_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainAppConfig(AppConfig):
-    name = 'main_app'
-
-def ready(self):
-    import signals

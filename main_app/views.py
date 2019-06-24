79b41915bd674dfeede875cd5b9212164bf0cba6
Andrew Williamson <acwilliamson419@gmail.com>
2019-06-24 09:25:54 -0700

    Removes profile photos until fix is found

--- main_app/views.py
+++ main_app/views.py
@@ -14,7 +14,7 @@ from django.db.models.signals import post_save
 import uuid
 import boto3
 from .forms import ProfileForm, UserForm, TripForm
-from .models import Profile, Drug, Effect, User_Drug_Effects, User, Trip_Report, ProfilePhoto
+from .models import Profile, Drug, Effect, User_Drug_Effects, User, Trip_Report #ProfilePhoto
 
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'ttitd'
@@ -28,7 +28,7 @@ def home(request):
 def profile(request):
     profile = request.user.profile
     user_id = request.user.id
-    avatar = ProfilePhoto.objects.get(profile=profile)
+    #avatar = ProfilePhoto.objects.get(profile=profile)
     return render(request, 'profile/detail.html', {
       'user_id': user_id,
       'p': profile,
